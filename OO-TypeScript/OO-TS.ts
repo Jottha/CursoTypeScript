088a3fd58df1765d3018352ac9a3be02e7feed9e
Jailton <jottha_2006@hotmail.com>
2018-09-12 14:13:09 -0300

    Interfaces

--- OO-TypeScript/OO-TS.ts
+++ OO-TypeScript/OO-TS.ts
@@ -1,4 +1,12 @@
-class NomeDaClasse {
+interface IInterface{
+    soma(a, b): number;
+}
+
+interface IInterface2 extends IInterface{
+
+}
+
+class NomeDaClasse implements IInterface, IInterface2{
     private atributo: number;
     public atributo2: boolean;
     protected atributo3: Object;
@@ -7,6 +15,9 @@ class NomeDaClasse {
         this.atributo2 = parametros;
     }
 
+    soma(a, b){
+        return a + b;
+    }
 
     imprimir(message: string): void {
         if(this.atributo2){
