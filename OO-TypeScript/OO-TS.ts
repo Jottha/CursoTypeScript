127d647f6ac643def23d1da14a96a18f3a3fe879
Jailton <jottha_2006@hotmail.com>
2018-09-12 14:14:45 -0300

    Enum

--- OO-TypeScript/OO-TS.ts
+++ OO-TypeScript/OO-TS.ts
@@ -1,33 +1,5 @@
-interface IInterface{
-    soma(a, b): number;
+enum Cores{
+    VERMELHOR, AZUL,AMARELO
 }
 
-interface IInterface2 extends IInterface{
-
-}
-
-class NomeDaClasse implements IInterface, IInterface2{
-    private atributo: number;
-    public atributo2: boolean;
-    protected atributo3: Object;
-
-    constructor(parametros: boolean){
-        this.atributo2 = parametros;
-    }
-
-    soma(a, b){
-        return a + b;
-    }
-
-    imprimir(message: string): void {
-        if(this.atributo2){
-            console.log(message);
-        }
-    }
-}
-
-const novo = new NomeDaClasse(true);
-
-novo.imprimir('Hello, World');
-
-export default NomeDaClasse;
+let cor: Cores = Cores.AZUL;
